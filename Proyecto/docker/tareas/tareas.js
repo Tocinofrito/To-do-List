6d43e141c3cd4b4de1c7714d2f9ba7a0b971af55
Tocinofrito <chichamalo@gmail.com>
2023-12-08 21:22:31 -0600

    Trabajar consultas tareas

--- Proyecto/docker/tareas/tareas.js
+++ Proyecto/docker/tareas/tareas.js
@@ -1,78 +1,69 @@
+//Declaramos la variable de express para el servidor
 const express = require('express')
+//Variable para manejo de la bd postgres
 const { Client } = require('pg')
+//Variables para manejar JSON y usar el archivo config y jsonwebtoken para seguridad
 const fs = require('fs')
+//jsonwebtoken es un mecanismo para propagar información entre dos partes de forma segura
 const wt = require('jsonwebtoken')
 
+//Creamos variable app para manejarr el sb y el puero en el que se manejará
+
 const app = express()
-const port = 3000
-
-const configData = fs.readFileSync('config.json', 'utf8') // Ler el archivo de configuración de forma síncrona
-
-if (!configData) {
+//Variable de cliente para la conexión a la base de datos
+let client
+//Se hace de manera síncrona debido a que debe tener los datos para configurara la conexión a la base de datos antes
+//De ejecutar esa parte de código
+const configData = fs.readFileSync('Proyecto/config/config-tareas.json', 'utf-8')
+//Conprobamos la existencia del archvio
+if(!configData){
   console.error('Error al leer el archivo de configuración')
   process.exit(1)
 }
+//Al hacer parse al JSON podemos acceder a sus valores con el .
+const config = JSON.parse(configData)
 
-const config = JSON.parse(configData) // Parsear los datos del archivo
-
-// Conectar a la base de datos
-const client = new Client({
+//Conexión a la bse de datos
+client = new Client({
   user: config.user,
   host: config.host,
   database: config.database,
   password: config.password,
   port: config.port
 })
-
-client.connect((err) => {
-  if (err) {
+//Comprobamos conexión a bd
+client.connect((err) =>{
+  if(err){
     console.error('Error al conectar a la base de datos:', err)
     process.exit(1)
   }
   console.log('Conectado a la base de datos')
 })
-
-app.get('/consulta/:data?', (req, res) => {
-  const SecretKey = config.secretKey // Utilizar la clave secreta desde el archivo de configuración
+//Ruta a llamar para lista de tareas por usuario
+//Parámetros en la ruta jwt:
+app.get('/consulta/:data?',(req, res) =>{
+  const SecretKey = config.SecretKey
   const token = req.params.data
-
-  if (!token) {
-    return res.status(401).send('Token JWT no proporcionado')
+  if(!token){
+    return res.status(401).sendStatus('Token JWT no proporcionado')
   }
-
-  wt.verify(token, SecretKey, (err, decoded) => {
-    if (err) {
-      console.error('Error al verificar token:', err)
-      return res.status(401).send('Token JWT no válido')
+  //Comprobamos el token sea correcto
+  wt.verify(token, SecretKey, (err, decoded) =>{
+    if(err){
+      console.error('Error al verificar el token:',err)
+      return res.status(401).sendStatus('Token JWT no válido')
     }
     const tipo = decoded.tipo
     const id = decoded.id
-
-    // Realizar consultas a la base de datos aquí
-    if (tipo === '1') {
-      const query = 'SELECT * FROM cat_estados'
-      client.query(query, (err, result) => {
-        if (err) {
-          console.error('Error en la consulta: ', err)
-          res.status(500).json({ error: 'Error en la consulta' })
-        } else {
-          res.json(result.rows)
-        }
-      })
-    } else if (tipo === '2' && id) {
-      const query = 'SELECT * FROM cat_estados WHERE id_estado = $1'
-      client.query(query, [id], (err, result) => {
-        if (err) {
-          console.error('Error en la consulta: ', err)
-          res.status(500).json({ error: 'Error en la consulta' })
-        } else {
-          res.json(result.rows)
-        }
-      })
-    }
   })
-})
-
-app.listen(port, () => {
-  console.log('Iniciado microservicio puerto: ', port)
+  //Realizamos la consulta y retornamos eljson
+  switch (tipo === '1') {
+    case value:
+      //Consulta de las tareas de todos los usuarios
+      const query = "SELECT * FROM tasks"
+      break;
+  
+    default:
+      break;
+  }
 })
